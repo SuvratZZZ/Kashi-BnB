ac982c712c3a1b4e3373b9eb8ae1db98b57f3f9e
SuvratZZZ <singhsubrat35@gmail.com>
2025-03-15 21:51:08 +0530

    complete signup login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,10 +1,38 @@
 import { PrismaClient } from '@prisma/client'
 import cryptoRandomString from 'crypto-random-string';
+import { sendEmail } from '../utils/mail.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const prisma = new PrismaClient();
 
 export const loginControl = async (req,res,nex)=>{
-}
+    try{
+        const user = await prisma.users.findUnique({
+            where:{
+                email: req.body.email
+            }
+        })
+        if(bcrypt.compare(req.body.password,user.password)){
+            const token = await jwt.sign(user,process.env.JWT_SEX);
+            return res.status(200).json({
+                message : "logged in",
+                token
+            });
+        }
+        else{
+            return res.status(411).json({
+                message : "wrong password"
+            });
+        }
+    }
+    catch(e){
+        return res.status(411).json({
+            message : "error logingin",
+            e
+        });
+    }
+};
 
 export const verification = async (req,res,nex)=>{
     try{
@@ -74,7 +102,8 @@ export const signupControl = async (req,res,nex)=>{
 
         const token = cryptoRandomString({length:15});
         console.log("token is : ",token);
-
+        req.body.password= await bcrypt.hash(req.body.password,10);
+        console.log("salt",req.body.password);
         if(!user){
             user = await prisma.users.create({
                 data: {
@@ -89,11 +118,13 @@ export const signupControl = async (req,res,nex)=>{
                     email: req.body.email
                 },
                 data:{
+                    password : req.body.password,
                     token: token,
                 }
             })
         }
 
+        await sendEmail(req.body.email,token);
 
         return res.status(201).json({
             message:"verify user to finsish",
--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,7 @@ app.use('/api/v1/user',userRouter);
 // app.use('/api/v1/restr');
 // app.use('/api/v1/hotel');
 
+
 app.listen(3000,()=>{
     console.log("app running on");
 })
