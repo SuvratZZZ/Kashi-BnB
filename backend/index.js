bdcf6bc9de3e01e2e8c97f54667f31ad683ec57c
SuvratZZZ <singhsubrat35@gmail.com>
2025-03-21 23:50:40 +0530

    req routes

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { limiter } from './middleware/auth.js';
 import userRouter from './routes/user.js';
+import cors from 'cors'
 
 const app = express();
 
+app.use(cors());
 app.use(limiter);
 app.use(express.json());
 app.use('/api/v1/user',userRouter);
