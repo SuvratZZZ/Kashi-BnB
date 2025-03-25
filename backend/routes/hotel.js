2a3dce7528e6bd7276bf0e7bccbb49cb48a7e29d
SuvratZZZ <singhsubrat35@gmail.com>
2025-03-25 18:29:59 +0530

    fixed hotel routes

--- backend/routes/hotel.js
+++ backend/routes/hotel.js
@@ -1,21 +1,22 @@
 import { prisma } from "../utils/client";
 import { Router } from "express";
+import { getHotels,getUniqueHotel } from '../controllers/hotel.js';
 
 const router = Router();
 
-router.use('/admin',authorisation,isAdmin,adminRouter);
+// router.use('/admin',authorisation,isAdmin,adminRouter);
 
 // unauthorised
 router.get('/hotel/:uid',getUniqueHotel);
 router.get('/hotels',getHotels);
 
 // user-authorised
-router.post('/upgrade_request',authorisation,makeRequest);
-router.get('/signup/verify/',verification);
-router.get('/check',checkControl);
-router.get('/auth-check',authorisation,isAdmin,checkControl);
-router.get('/admin-check',authorisation,isAdmin,checkControl);
-router.get('/hotel-check',authorisation,hasHotel,checkControl);
+// router.post('/upgrade_request',authorisation,makeRequest);
+// router.get('/signup/verify/',verification);
+// router.get('/check',checkControl);
+// router.get('/auth-check',authorisation,isAdmin,checkControl);
+// router.get('/admin-check',authorisation,isAdmin,checkControl);
+// router.get('/hotel-check',authorisation,hasHotel,checkControl);
 
 // hoteler-authorised
 
